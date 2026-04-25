7724782a7c38b2194fdb1ec1f8bddfb33d5ce43a
steveberndt90-eng <steve.berndt90@gmail.com>
2026-04-25 14:11:09 +0200

    Add push notification handling to service worker

--- sw.js
+++ sw.js
@@ -9,3 +9,19 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request).catch(() => new Response('App ist offline')));
 });
+
+self.addEventListener('push', function(e) {
+  const data = e.data ? e.data.json() : { title: 'Dex Messager', body: 'Neue Nachricht' };
+  const options = {
+    body: data.body,
+    icon: 'icon-192.png',
+    badge: 'icon-192.png',
+    vibrate: [100, 50, 100]
+  };
+  e.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener('notificationclick', function(e) {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/'));
+});
